c4a09bcf8588f4ff60d93b3e9504c5040079fb96
Kaustubh Jogle <kaustubhrjogle@gmail.com>
2026-02-21 18:26:51 +0530

    Add Hyperlinks

---
+++ src/utlis/Links.ts
@@ -0,0 +1,5 @@
+export const HYPERLINKS = [
+    { name: "GitHub", url: "https://github.com/kaustubhjogle", label: "github.com/kaustubhjogle", target: "_blank" },
+    { name: "LinkedIn", url: "https://linkedin.com/in/kaustubhjogle", label: "linkedin.com/in/kaustubhjogle", target: "_blank" },
+    { name: "Email", url: "mailto:hello@kaustubhjogle.com", label: "kaustubhrjogle@gmail.com", target: undefined },
+]   
